d1d5677455e13659eff7ed5561ab0ab63212b018
Noor <>
2023-05-11 14:31:07 +0300

    edit interface function

--- app/src/main/java/com/noor/fakeshopestore/data/remote/ShopApiService.kt
+++ app/src/main/java/com/noor/fakeshopestore/data/remote/ShopApiService.kt
@@ -2,14 +2,13 @@ package com.noor.fakeshopestore.data.remote
 
 import com.noor.fakeshopestore.domain.entities.main.Cart
 import com.noor.fakeshopestore.domain.entities.main.Product
+import com.noor.fakeshopestore.domain.entities.wrappers.Response
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.PATCH
@@ -22,16 +21,16 @@ import retrofit2.http.Query
 interface ShopApiService {
 
     @GET("products")
-    fun getProducts(@Query("limit") limit: Int?): Single<Response<List<Product>>>
+    fun getProducts(@Query("limit") limit: Int?): Single<Response<Product>>
 
     @GET("products/{id}")
     fun getSingleProduct(@Path("id") id: Int): Single<Response<Product>>
 
     @GET("products/categories")
-    fun getCategories(): Single<Response<List<String>>>
+    fun getCategories(): Single<Response<String>>
 
     @GET("/products")
-    fun getProductsByCategory(@Query("category") category: String): Single<Response<List<Product>>>
+    fun getProductsByCategory(@Query("category") category: String): Single<Response<Product>>
 
     @GET("/carts")
     fun getUserCartProducts(@Query("userId") userId: Int): Single<Response<List<Cart>>>
